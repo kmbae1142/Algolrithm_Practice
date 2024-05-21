d8bb7a6f70b5e4e42a6c5d4ee5cf4a0ea319e9c0
kmbae1142 <102423525+kmbae1142@users.noreply.github.com>
2024-05-21 17:52:12 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.py
@@ -0,0 +1,24 @@
+num = int(input())
+
+if num == 1:
+    print("12 1600")
+elif num == 2:
+    print("11 894")
+elif num == 3:
+    print("11 1327")
+elif num == 4:
+    print("10 1311")
+elif num == 5:
+    print("9 1004")
+elif num == 6:
+    print("9 1178")
+elif num == 7:
+    print("9 1357")
+elif num == 8:
+    print("8 837")
+elif num == 9:
+    print("7 1055")
+elif num == 10:
+    print("6 556")
+else:
+    print("6 773")

0e5885a8fbdf0da12630c7bbfe86e878aceb0d68
kmbae1142 <102423525+kmbae1142@users.noreply.github.com>
2024-05-26 22:42:20 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,18 @@
+num = int(input())
+
+for i in range(num):
+    semi = input()
+    if semi == "Algorithm":
+        print("204")
+    elif semi == "DataAnalysis":
+        print("207")
+    elif semi == "ArtificialIntelligence":
+        print("302")
+    elif semi == "CyberSecurity":
+        print("B101")
+    elif semi == "Network":
+        print("303")
+    elif semi == "Startup":
+        print("501")
+    elif semi == "TestStrategy":
+        print("105")

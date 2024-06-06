04f4494832b9bc08ce17153307c45c40d2d9d178
kmbae1142 <102423525+kmbae1142@users.noreply.github.com>
2024-06-06 09:26:41 +0900

    [Bronze II] Title: 피보나치 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -0,0 +1,21 @@
+def pivot(n):
+
+    if n == 0: return 0
+    if n == 1: return 1
+    if n == 2: return 1
+
+    fn1 = 1
+    fn2 = 1
+    fn = 0
+
+    for i in range(n - 2):
+        fn = fn1 + fn2
+        fn1 = fn2
+        fn2 = fn
+
+    return fn
+
+
+num = int(input())
+
+print(pivot(num))

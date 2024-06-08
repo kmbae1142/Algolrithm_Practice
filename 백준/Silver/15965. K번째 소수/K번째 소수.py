09c849732237f4302da0d556deddfa7e013f4329
kmbae1142 <102423525+kmbae1142@users.noreply.github.com>
2024-06-08 10:27:18 +0900

    [Silver II] Title: K번째 소수, Time: 3772 ms, Memory: 187372 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.py
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -1,11 +1,13 @@
 n = int(input())
-a = [False, False] + [True]*(500000 - 1)
+a = [False, False] + [True]*(10000000 - 1)
 primes = []
 
-for i in range(2, 500001):
+for i in range(2, 10000001):
     if a[i]:
+        if len(primes) == n:
+            break
         primes.append(i)
-        for j in range(2*i, 500001, i):
+        for j in range(2*i, 10000001, i):
             a[j] = False
 
 print(primes[n - 1])

4cd43bbe7164a0228ac18e8a7675a3b704b785e8
kmbae1142 <102423525+kmbae1142@users.noreply.github.com>
2024-07-19 00:19:41 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main() {
+
+    int N, M;
+    scanf("%d %d", &N, &M);
+    int matrix1[N][M];
+    int matrix2[N][M];
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            scanf("%d ", &matrix1[i][j]);
+        }
+        //printf("\n");
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            scanf("%d ", &matrix2[i][j]);
+        }
+        //printf("\n");
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            printf("%d ", matrix1[i][j] + matrix2[i][j]);
+        }
+        printf("\n");
+    }
+
+
+}

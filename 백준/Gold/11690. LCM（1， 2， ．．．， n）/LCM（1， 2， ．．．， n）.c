95b7c6f28850051c6f40ebdd61b241d3ced317ab
kmbae1142 <102423525+kmbae1142@users.noreply.github.com>
2024-07-25 23:03:57 +0900

    [Gold IV] Title: LCM(1, 2, ..., n), Time: 1316 ms, Memory: 98784 KB -BaekjoonHub

---
+++ 백준/Gold/11690. LCM（1， 2， ．．．， n）/LCM（1， 2， ．．．， n）.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool Primes[100000001];
+
+void prime_numbers(bool arr[], int len) {
+
+	for (int i = 2; i < (int)sqrt(len) + 1; ++i) {
+
+		if (arr[i] == false) {
+			continue;
+		}
+
+		for (int j = i * i; j < len + 1; j += i) {
+			arr[j] = false;
+		}
+
+	}
+
+}
+
+int main() {
+
+	int num;
+	unsigned long long result = 1;
+	unsigned long long rem = 1ULL << 32;
+	scanf("%d", &num);
+
+	for (int i = 0; i <= num; ++i) {
+		Primes[i] = true;
+	}
+
+	prime_numbers(Primes, num);
+
+	for (int i = 2; i <= num; ++i) {
+
+		if (Primes[i] != false) {
+			unsigned long long temp = i;
+
+			while (temp * i <= (unsigned long long)num)
+				temp *= i;
+
+			result *= temp;
+			result %= rem;
+		}
+	}
+
+	printf("%llu", result);
+}

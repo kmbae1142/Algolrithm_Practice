b01a3d613f341d0e1a6919b646ce438f8ec4ccc2
kmbae1142 <102423525+kmbae1142@users.noreply.github.com>
2024-07-29 22:07:28 +0900

    [Silver IV] Title: 접미사 배열, Time: 12 ms, Memory: 2092 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.c
@@ -0,0 +1,40 @@
+typedef struct {
+
+	char suffix[1002];
+
+}Suffix;
+
+Suffix Sfx[1000];
+
+int compare(const Suffix* a, const Suffix* b) {
+
+	return strcmp(a->suffix, b->suffix);
+
+}
+
+int main() {
+
+	char str[1002];
+	int i, j, len;
+
+	scanf("%s", str);
+
+	len = strlen(str);
+
+	for (i = 0; i < len; ++i) {
+		strcpy(Sfx[i].suffix, str);
+		for (j = 0; j < strlen(str) - 1; ++j) {
+			str[j] = str[j + 1];
+		}
+		str[j] = '\0';
+	}
+
+	qsort(Sfx, len, sizeof(Suffix), compare);
+
+	for (i = 0; i < len; ++i) {
+		printf("%s\n", Sfx[i].suffix);
+	}
+
+	return 0;
+
+}

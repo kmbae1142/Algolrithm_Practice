8ec6e3ff0a16817996e02f61741c74fa85a855c8
kmbae1142 <102423525+kmbae1142@users.noreply.github.com>
2024-08-12 19:56:25 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0); 
+	int num;
+	int n;
+	priority_queue<int> q;
+
+	cin >> num;
+
+	for (int i = 0; i < num; ++i) {
+		cin >> n;
+		if (!n) {
+			if (q.empty()) {
+				cout << "0" << '\n';
+			}
+			else {
+				cout << q.top() << '\n';
+				q.pop();
+			}
+		}
+		else {
+			q.push(n);
+		}
+	}
+	
+}

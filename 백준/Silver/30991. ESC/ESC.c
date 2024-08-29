df27798d812ce0f11a3e2997bc2c0d82f1db9c06
kmbae1142 <102423525+kmbae1142@users.noreply.github.com>
2024-08-29 15:53:58 +0900

    [Silver I] Title: ESC, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/30991. ESC/ESC.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int n;
+	long long c[32];
+	c[0] = 1;
+	long long b = 1;
+	scanf("%d", &n);
+	for (int i = 1; i < n; i++) {
+		c[i] = c[i - 1] - 4 * b;
+		b = b + c[i - 1];
+	}
+	printf("%lld", c[n - 1]);
+}

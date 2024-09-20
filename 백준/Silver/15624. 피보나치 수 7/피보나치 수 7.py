c964bfc4bc735e6d9714201fc368a21a60e59985
kmbae1142 <102423525+kmbae1142@users.noreply.github.com>
2024-09-20 19:12:51 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 28 ms, Memory: 70668 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -0,0 +1,15 @@
+def pivo(n):
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    if n > 0:
+        dp[1] = 1
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] % 1000000007 + dp[i - 2] % 1000000007
+
+    return dp[n]
+
+
+num = int(input())
+
+print(pivo(num) % 1000000007)

8c9a3f140368cede585c63dbc7982248b8b5c9b3
kmbae1142 <102423525+kmbae1142@users.noreply.github.com>
2024-09-21 21:38:57 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 6412 ms, Memory: 8924 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.c
@@ -0,0 +1,32 @@
+int num[1000000];
+
+int compare(const void* _n1, const void* _n2) {
+
+    int* n1 = (int*)_n1;
+    int* n2 = (int*)_n2;
+
+    if (*n1 > *n2)
+        return 1;
+    else if (*n1 < *n2)
+        return -1;
+    else
+        return 0;
+
+}
+
+int main() { 
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &num[i]);
+    }
+
+    qsort(num, n, sizeof(int), compare);
+
+    for (int i = 0; i < n; ++i) {
+        printf("%d\n", num[i]);
+    }
+
+}

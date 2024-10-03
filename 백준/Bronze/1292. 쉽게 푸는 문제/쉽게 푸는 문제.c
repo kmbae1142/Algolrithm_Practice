44b64ad7359de7678415e6a1aaaeb60f25a14923
kmbae1142 <102423525+kmbae1142@users.noreply.github.com>
2024-10-03 21:47:06 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.c
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.c
@@ -3,31 +3,22 @@
 int main() {
 
 	int num[1000] = { 0, };
-	int numc = 1, numc2 = 1, i = 0, j = 0;
-	int temp = numc + i;
-	int num1, num2, sum = 0;
+	int A, B, i, j = 0;
+	int count = 0, current_index = 0, sum = 0;
+	scanf("%d %d", &A, &B);
 
 	while (1) {
-
-		if (temp > 999)
-			temp = 999;
-
-		for (j = i; j <= temp; ++j) {
-			num[j] = numc;
+		for (i = j; i <= j + current_index; i++) {
+			if (count == B) break;
+			num[i] = current_index + 1;
+			count++;
 		}
-
-		if (temp == 999)
-			break;
-
-		numc++;
-		i += numc - 1;
-		temp = numc + i;
-				
+		if (count == B) break; 
+		current_index++;
+		j = i;
 	}
-	
-	scanf("%d %d", &num1, &num2);
 
-	for (i = num1 - 1; i < num2; i++) {
+	for (i = A - 1; i < B; i++) { 
 		sum += num[i];
 	}
 

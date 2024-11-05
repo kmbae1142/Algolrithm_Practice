38233288cdd09d5fa3a87a635435a5753a54be70
kmbae <102423525+kmbae1142@users.noreply.github.com>
2024-11-05 13:05:53 +0900

    [Bronze III] Title: 홀수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.c
+++ 백준/Bronze/2576. 홀수/홀수.c
@@ -1,40 +1,23 @@
 #include <stdio.h>
 
-
-int smallest(int a[], int i) {
-
-    int j, min = 100;
-
-    for (j = 0; j < i; j++) {
-        if (a[j] % 2 == 1) {
-            if (a[j] < min)
-                min = a[j];
-        }
-    }
-
-    return min;
-}
-
 int main() {
-
-    int num[7];
-    int rem = 0;
-    int sum = 0;
-
+    
+    int num, min = 101, sum = 0;
+    
     for (int i = 0; i < 7; i++) {
-        scanf("%d", &num[i]);
-        if (num[i] % 2 != 0) {
-            sum += num[i];
+        scanf("%d", &num);
+        if (num % 2 != 0) {
+            sum += num;
+            if (min > num)
+                min = num;
         }
     }
-
-    if (sum == 0) {
-        printf("%d", -1);
+    
+    if (sum) {
+        printf("%d\n%d", sum, min);
     }
     else {
-        printf("%d\n", sum);
-        printf("%d", smallest(num, 7));
+        printf("-1");
     }
-
-
+    
 }

0053a13e72dfd15ef0f8f3db0d6ef4f5502c3959
kmbae <102423525+kmbae1142@users.noreply.github.com>
2024-11-17 13:49:00 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,16 @@
+n = int(input())
+dic = {
+    1: (11, 'A B C D E F G H J L M'),
+    2: (9, 'A C E F G H I L M'),
+    3: (9, 'A C E F G H I L M'),
+    4: (9, 'A B C E F G H L M'),
+    5: (8, 'A C E F G H L M'),
+    6: (8, 'A C E F G H L M'),
+    7: (8, 'A C E F G H L M'),
+    8: (8, 'A C E F G H L M'),
+    9: (8, 'A C E F G H L M'),
+    10: (8, 'A B C F G H L M')
+}
+
+print(dic[n][0])
+print(dic[n][1])

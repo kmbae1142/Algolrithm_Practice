c21937a6b2e4504b73576209b0dc4768419bc0f4
kmbae <102423525+kmbae1142@users.noreply.github.com>
2024-11-25 08:55:18 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,16 @@
+num = int(input())
+
+if num >= 620 and num <= 780:
+    print("Red")
+elif num >= 590:
+    print("Orange")
+elif num >= 570:
+    print("Yellow")
+elif num >= 495:
+    print("Green")
+elif num >= 450:
+    print("Blue")
+elif num >= 425:
+    print("Indigo")
+else:
+    print("Violet")
